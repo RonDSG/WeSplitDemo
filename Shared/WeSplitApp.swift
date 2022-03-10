bcb34d1d2dc31417541e54e71e1cfb357484c927
RonDSG <101124950+RonDSG@users.noreply.github.com>
2022-03-10 08:42:23 -0500

    Initial Commit

---
+++ Shared/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  Shared
+//
+//  Created by Jones, Ronald on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
